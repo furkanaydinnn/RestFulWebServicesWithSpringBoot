dd55d0113a9552c03313bbaea84ef8b60fde4c4a
Furkan Aydın <furkanaydin@Furkans-MacBook-Pro.local>
2020-04-25 11:27:56 +0300

    some basic rest web services

---
+++ restfulWebServices/src/main/java/com/web/services/restfulWebServices/HelloWorldBean.java
@@ -0,0 +1,24 @@
+package com.web.services.restfulWebServices;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+
+}
