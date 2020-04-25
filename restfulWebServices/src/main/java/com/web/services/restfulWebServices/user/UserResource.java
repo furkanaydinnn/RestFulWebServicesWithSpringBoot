4c2e2085a4f45407aba10da8b460da7bebcaeb4b
Furkan Aydın <furkanaydin@Furkans-MacBook-Pro.local>
2020-04-25 14:38:58 +0300

    customize exception handling

--- restfulWebServices/src/main/java/com/web/services/restfulWebServices/user/UserResource.java
+++ restfulWebServices/src/main/java/com/web/services/restfulWebServices/user/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,30 +16,45 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
-	
+
 	@Autowired
 	private UserDaoService service;
-	
-	@GetMapping(path="/users")
-	public List<User> retrieveAllUsers(){
+
+	@GetMapping(path = "/users")
+	public List<User> retrieveAllUsers() {
 		return service.findAll();
 	}
-	
-	@GetMapping(path="/users/{id}")
+
+	@GetMapping(path = "/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return service.findOne(id);
+		User user = service.findOne(id);
+		if (user == null) {
+			throw new UserNotFoundException("id : " + id);
+		}
+		return user;
 	}
-	
-	/*@GetMapping("/users")
-	public User retrieveUserByRequestParam(@RequestParam("id") int id) {
-		return service.findOne(id);
-	}*/
-	
+
+	/*
+	 * @GetMapping("/users") public User
+	 * retrieveUserByRequestParam(@RequestParam("id") int id) { return
+	 * service.findOne(id); }
+	 */
+
 	@PostMapping("/users")
 	public ResponseEntity<Object> saveUser(@RequestBody User user) {
 		User savedUser = service.save(user);
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
 		return ResponseEntity.created(location).build();
+	}
+
+	@DeleteMapping(path = "/users/{id}")
+	public void deleteUserById(@PathVariable int id) {
+		User user = service.deleteById(id);
+		if (user == null) {
+			throw new UserNotFoundException("id : " + id);
+		}
+
 	}
 
 }
