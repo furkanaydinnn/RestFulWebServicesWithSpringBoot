ec5972088a23d8c140cdba0f8c798989b37061e4
Furkan Aydın <furkanaydin@Furkans-MacBook-Pro.local>
2020-05-01 11:00:08 +0300

    Versioning with header and ContentNegotiation Approach

--- restfulWebServices/src/main/java/com/web/services/restfulWebServices/versioning/PersonVersioningController.java
+++ restfulWebServices/src/main/java/com/web/services/restfulWebServices/versioning/PersonVersioningController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 	
+	
+	/* **** URI Versioning   ***** */
 	@GetMapping("v1/person")
 	public PersonV1 getPersonV1() {
 		return new PersonV1("Furkan Aydin");
@@ -14,6 +16,39 @@ public class PersonVersioningController {
 	@GetMapping("v2/person")
 	public PersonV2 getPersonV2() {
 		return new PersonV2(new Name("Furkan","Aydin"));
+	}
+	
+	/* ***** Request Param Versioning ***** */
+	@GetMapping(value="person", params = "version=1" )
+	public PersonV1 paramV1() {
+		return new PersonV1("Furkan Aydin");
+	}
+	
+	@GetMapping(value="person", params = "version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Furkan","Aydin"));
+	}
+	
+	/* **** Header Approach ****** */
+	@GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
+	public PersonV1 headerV1() {
+		return new PersonV1("Furkan Aydin");
+	}
+
+	@GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
+	public PersonV2 headerV2() {
+		return new PersonV2(new Name("Furkan", "Aydin"));
+	}
+	
+	/* ***** Content Negotiation Approach ***** */
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("Furkan Aydin");
+	}
+
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 producesV2() {
+		return new PersonV2(new Name("Furkan", "Aydin"));
 	}
 
 }
